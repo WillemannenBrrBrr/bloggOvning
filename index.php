ac63cf295dae80d9ef8b7caccbdeac6bcf00ed2b
William Danielsson <William.Danielsson02@gmail.com>
2021-08-02 01:59:34 +0200

    testar connectionen

--- index.php
+++ index.php
@@ -5,9 +5,19 @@ $app->renderHeader("Skapa en användare");
 
 $form = $app->getForm();
 
+if(!empty($_POST))
+{
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $query = "INSERT INTO users (`username`, `password`) VALUES ($username, $password)";
+    $app->getdb()->query($query);
+}
+
 $form->openForm();
 $form->createInput("text", "username", "Användarnamn");
 $form->createInput("password", "password", "Lösenord");
+$form->createSubmit("Skapa");
 $form->closeForm();
 
 $app->renderFooter();
