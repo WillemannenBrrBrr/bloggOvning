b6e2ecbbfa7db7cb1cdc76b0135c3c0b742e02a5
William Danielsson <William.Danielsson02@gmail.com>
2021-10-09 00:16:39 +0200

    För att svara på en kommentar så behöver man fylla i att svara och att kommentera. har ingen bra lösning för detta

--- include/CComments.php
+++ include/CComments.php
@@ -18,6 +18,19 @@ class CComments
                     <p class="commenter"><a href="profile.php?id=<?php echo($commentData["userId"]) ?>"><?php echo($commenter["username"]) ?></a></p>
                     <p class="commentDate"><?php echo($commentDate) ?></p>
                 </div>
+                <div class="replies">
+                    <?php
+                        if(isLoggedIn())
+                        {
+                            $this->renderReplyForm($commentData);
+                        }
+                        else
+                        {
+                            echo("Du behöver vara inloggad för att svara </br>");
+                        }
+                        $this->selectAndRenderAllReplies($commentData);
+                    ?>
+                </div>
                 <?php
                     if(isLoggedIn())
                     {
@@ -51,6 +64,66 @@ class CComments
         {
             echo("Var den första och kommetera på det här inlägget");
         }
+    }
+
+    private function renderReply(array $replyData, array $commentData)
+    {
+        $replyer = $this->m_app->getDB()->selectByField("users", "id", $replyData["userId"]);
+        $replyDate = date("d-m-Y H:i", $replyData["date"]);
+        ?>
+            <div class="replies">
+                <p class="replyText"><?php echo(nl2br($replyData["text"])) ?></p>
+                <div class="footer">
+                    <p class="replyer"><a href="profile.php?id=<?php echo($replyData["userId"]) ?>"><?php echo($replyer["username"]) ?></a></p>
+                    <p class="replyDate"><?php echo($replyDate) ?></p>
+                </div>
+                <?php
+                    /* if(isLoggedIn())
+                    {
+                        if($_SESSION["userData"]["id"] == $commentData["userId"] || $_SESSION["userData"]["id"] == $postData["userId"])
+                        {
+                            ?>
+                                <a href="deleteComment.php?id=<?php echo($commentData["id"]) ?>&postId=<?php echo($commentData["postId"]) ?>">Ta bort</a>
+                            <?php
+                        }
+                    } */
+                ?>
+            </div>
+        <?php
+    }
+
+    private function selectAndRenderAllReplies(array $commentData)
+    {
+        $query ="SELECT * FROM replies WHERE commentId = " . $commentData["id"] . "";
+        $result = $this->m_app->getDB()->query($query);
+        $numberOfReplies = $result->num_rows;
+
+        if($numberOfReplies > 0)
+        {
+            for($i = $numberOfReplies; $i > 0; $i--)
+            {
+                $reply = $result->fetch_assoc();
+                $this->renderReply($reply, $commentData);
+            }
+        }
+    }
+
+    private function renderReplyForm(array $commentData)
+    {
+        if(!empty($_POST))
+        {
+            $reply = $_POST["reply"];
+            $replier = $_SESSION["userData"]["id"];
+            $commentId = $commentData["id"];
+
+            $data = ["text" => $reply, "date" => time(), "userId" => $replier, "commentId" => $commentId];
+            $this->m_app->getDB()->insert("replies", $data);
+        }
+
+        $this->m_app->getForm()->openForm();
+        $this->m_app->getForm()->createInput("text", "reply", "Svara");
+        $this->m_app->getForm()->createSubmit("Skicka");
+        $this->m_app->getForm()->closeForm();
     }
 
     //////////////////////////////////////////////////
